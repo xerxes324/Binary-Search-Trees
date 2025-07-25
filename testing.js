eabdafc9c2ec289f2bb2a29331dad482d8429fb5
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-07-25 15:08:04 +0530

    Testing

--- testing.js
+++ testing.js
@@ -1,5 +1,17 @@
 import { Tree, prettyPrint } from "./BST.js";
 
-let array1 = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+let array1 = [43,4,2,3,5,19,184,66,3,33,8];
 let tree = new Tree(array1)
+tree.insert(tree.root, 195)
+console.log("Balanced : ",tree.isBalanced());
 prettyPrint(tree.root);
+
+tree.rebalance();
+prettyPrint(tree.root);
+console.log("Balanced now? : ", tree.isBalanced());
+console.log("inorder: ")
+tree.inOrderForEach((val)=>console.log(val), tree.root);
+console.log("Preorder:")    
+tree.preOrderForEach((val)=>console.log(val), tree.root);
+console.log("Postorder");
+tree.postOrderForEach((val)=> console.log(val), tree.root);
